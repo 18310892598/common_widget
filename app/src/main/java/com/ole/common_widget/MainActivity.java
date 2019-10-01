6ec6fd41d900cc110772ba42f4b117e0dfe7cd03
john <334786817@qq.com>
2019-10-01 22:15:57 +0800

    4、上传打通

--- app/src/main/java/com/ole/common_widget/MainActivity.java
+++ app/src/main/java/com/ole/common_widget/MainActivity.java
@@ -3,11 +3,14 @@ package com.ole.common_widget;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import ola.com.dialog.OleDialog;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        OleDialog.loggg();
     }
 }
---
+++ dialog/src/main/java/ola/com/dialog/OleDialog.java
@@ -0,0 +1,10 @@
+package ola.com.dialog;
+
+import android.util.Log;
+
+public class OleDialog {
+
+    public static void loggg(){
+        Log.e("orz-dialog","loggg");
+    }
+}
