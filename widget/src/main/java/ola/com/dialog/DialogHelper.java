335449179e18f1b4fbdc55fd4f78cbacac4d0fed
john <334786817@qq.com>
2019-10-03 16:08:52 +0800

    4、修改module名称

--- widget/src/main/java/ola/com/dialog/DialogHelper.java
+++ widget/src/main/java/ola/com/dialog/DialogHelper.java
@@ -25,7 +25,6 @@ public class DialogHelper {
 
 
     private static Dialog dialog;
-    private DialogListener dialogCancelListener;
 
     public void showDialog(Context context, String title, String content) {
         showDialog(context, title, content, null);
