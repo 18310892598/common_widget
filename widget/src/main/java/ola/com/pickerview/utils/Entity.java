47f5689e8947ee7d75dea78867dd92d5d185daf8
yangshangcai <>
2019-10-16 10:54:36 +0800

    bean

---
+++ widget/src/main/java/ola/com/pickerview/utils/Entity.java
@@ -0,0 +1,13 @@
+package ola.com.pickerview.utils;
+
+
+import java.io.Serializable;
+
+/**
+ * @author liujia
+ * 实体类
+ * 
+ */
+public abstract class Entity implements Serializable{
+    public int id;
+}
