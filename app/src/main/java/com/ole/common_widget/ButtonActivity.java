4e1543479ebf28c8660cc16e42eb37318d638163
yangshangcai <>
2019-12-05 11:56:58 +0800

    button可选择

---
+++ app/src/main/java/com/ole/common_widget/ButtonActivity.java
@@ -0,0 +1,32 @@
+package com.ole.common_widget;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author yang
+ */
+public class ButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button);
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.button2).setSelected(false);
+            }
+        });
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.button3).setSelected(!findViewById(R.id.button3).isSelected());
+            }
+        });
+    }
+}
