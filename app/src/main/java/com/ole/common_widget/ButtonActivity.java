43b6b867936b7716d92872cd62b6d6d59349f923
yangshangcai <>
2019-12-05 18:26:42 +0800

    button样式

--- app/src/main/java/com/ole/common_widget/ButtonActivity.java
+++ app/src/main/java/com/ole/common_widget/ButtonActivity.java
@@ -1,10 +1,14 @@
 package com.ole.common_widget;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import ola.com.button.SlideDraghelperView;
 
 /**
  * @author yang
@@ -43,5 +47,17 @@ public class ButtonActivity extends AppCompatActivity {
                 findViewById(R.id.button5).setSelected(!findViewById(R.id.button5).isSelected());
             }
         });
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.custom_navi_trip_opt, null);
+
+        this.addContentView(view,findViewById(R.id.layout).getLayoutParams());
+        ((SlideDraghelperView)findViewById(R.id.slide_right_view_draghelper)).setOnReleasedListener(new SlideDraghelperView.OnReleasedListener() {
+            @Override
+            public void onReleased(int left, int top, int right, int bottom) {
+                Toast.makeText(ButtonActivity.this,"滑动成功",Toast.LENGTH_SHORT).show();
+                ((SlideDraghelperView)findViewById(R.id.slide_right_view_draghelper)).layout( left,  top,  right,  bottom);
+            }
+        });
+
     }
 }
---
+++ widget/src/main/java/ola/com/button/SlideDraghelperView.java
@@ -0,0 +1,176 @@
+package ola.com.button;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.LinearLayout;
+
+import androidx.customview.widget.ViewDragHelper;
+
+/**
+ * Create by wyman
+ * Email  piitw@qq.com
+ * Create at 2019-05-31 10:17
+ * Desc :
+ */
+public class SlideDraghelperView extends LinearLayout {
+
+    private ViewDragHelper viewDragHelper;
+    private View child;
+    private Point childPosition = new Point();
+    private Point childEndPosition = new Point();
+    private OnReleasedListener onReleasedListener;
+    private int oldX;
+
+    public SlideDraghelperView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setTouchable(true);
+    }
+
+    public void setTouchable(boolean isTouch) {
+        if (isTouch) {
+            //新建viewDragHelper ,viewGroup, 灵敏度，回调(子view的移动)
+            viewDragHelper = ViewDragHelper.create(this, 1.0f, new ViewDragHelper.Callback() {
+                @Override
+                public boolean tryCaptureView(View child, int pointerId) {
+                    oldX = 0;
+                    return true;
+                }
+
+                @Override
+                public int clampViewPositionHorizontal(View child, int left, int dx) {
+                    oldX = left;
+                    return Math.max(0, left);
+                }
+
+                @Override
+                public void onViewReleased(View releasedChild, float xvel, float yvel) {
+                    if (oldX > getWidth() / 2) {
+                        TranslateAnimation translateAnimation =
+                                new TranslateAnimation(0, childEndPosition.x, childPosition.y, childPosition.y);
+                        translateAnimation.setDuration(300);
+                        child.startAnimation(translateAnimation);
+                        translateAnimation.setAnimationListener(new Animation.AnimationListener() {
+                            @Override
+                            public void onAnimationStart(Animation animation) {
+
+                            }
+
+                            @Override
+                            public void onAnimationEnd(Animation animation) {
+                                Handler handler = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        onReleasedListener.onReleased(0, 0, getWidth(), getHeight());
+                                    }
+                                }, 10);
+
+                            }
+
+                            @Override
+                            public void onAnimationRepeat(Animation animation) {
+
+                            }
+                        });
+                    } else {
+                        //滑动距离不够一半，回到初始位置
+                        viewDragHelper.settleCapturedViewAt(childPosition.x, childPosition.y);
+                        //反弹
+                        invalidate();
+                    }
+
+
+                    super.onViewReleased(releasedChild, xvel, yvel);
+                }
+
+                @Override
+                public void onViewCaptured(View capturedChild, int activePointerId) {
+                    super.onViewCaptured(capturedChild, activePointerId);
+                }
+
+
+                @Override
+                public void onViewPositionChanged(View changedView, int left, int top, int dx, int dy) {
+                    super.onViewPositionChanged(changedView, left, top, dx, dy);
+                }
+
+            });
+        } else {
+            viewDragHelper = ViewDragHelper.create(this, 1.0f, new ViewDragHelper.Callback() {
+                @Override
+                public boolean tryCaptureView(View child, int pointerId) {
+                    return false;
+                }
+            });
+        }
+    }
+
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        child = getChildAt(0);
+    }
+
+    /**
+     * 用viewDragHelper拦截-true
+     *
+     * @param ev
+     * @return
+     */
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return viewDragHelper.shouldInterceptTouchEvent(ev);
+    }
+
+    /**
+     * viewDragHelper拦截事件
+     *
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        try {
+            viewDragHelper.processTouchEvent(event);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return super.onTouchEvent(event);
+        }
+
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+        //定位左侧的坐标
+        childPosition.x = child.getLeft();
+        childPosition.y = child.getTop();
+
+        //定位右侧的坐标
+        childEndPosition.x = child.getRight();
+        childEndPosition.y = child.getTop();
+    }
+
+    @Override
+    public void computeScroll() {
+        super.computeScroll();
+        if (viewDragHelper.continueSettling(true)) {
+            invalidate();
+        }
+    }
+
+    public void setOnReleasedListener(OnReleasedListener onReleasedListener) {
+        this.onReleasedListener = onReleasedListener;
+    }
+
+    public interface OnReleasedListener {
+        void onReleased(int left, int top, int right, int bottom);
+    }
+}
