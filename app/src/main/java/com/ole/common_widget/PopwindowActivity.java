d2e1f7607ae8d3833f939e9dcda97895f0772daf
yangshangcai <yang@YangdeMacBook-Pro.local>
2020-02-12 01:12:34 +0800

    添加备注

--- app/src/main/java/com/ole/common_widget/PopwindowActivity.java
+++ app/src/main/java/com/ole/common_widget/PopwindowActivity.java
@@ -38,10 +38,10 @@ public class PopwindowActivity extends AppCompatActivity {
 
     private void showPopwindow(View v) {
         popupWindow = new CommonPopupWindow.Builder(this)
-                .setView(R.layout.home_show_choice_number_view)
-                .setWidthAndHeight(1, 0)
-                .setAnimationStyle(R.style.picker_view_slide_anim)
-                .setViewOnclickListener(new CommonPopupWindow.ViewInterface() {
+                .setView(R.layout.home_show_choice_number_view)//传入自定义布局
+                .setWidthAndHeight(1, 0)//设置布局宽高
+                .setAnimationStyle(R.style.picker_view_slide_anim)//设置显示动画
+                .setViewOnclickListener(new CommonPopupWindow.ViewInterface() {//设置自定义布局点击事件
                     @Override
                     public void getChildView(View view, int layoutResId) {
                         TextView choice_one = view.findViewById(R.id.choice_one);
@@ -65,10 +65,10 @@ public class PopwindowActivity extends AppCompatActivity {
                         close.setOnClickListener(view1 -> popupWindow.dismiss());
                     }
                 })
-                .setOutsideTouchable(true)
+                .setOutsideTouchable(true)//设置外部是否可触摸
                 .create();
 
-        popupWindow.showAtLocation(v, Gravity.BOTTOM, 0, 0);
+        popupWindow.showAtLocation(v, Gravity.BOTTOM, 0, 0);//设置显示位置
     }
 
 }
